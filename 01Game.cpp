856a91252f0462c80cbc56cd9571faba1f10b106
Krishna Sharma <rakesh7355249204@gmail.com>
2025-04-15 04:44:14 +0530

    01Game Changes

--- 01Game.cpp
+++ 01Game.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+#include <cstring>
 using namespace std;
+#define ll long long 
+#define vint vector<int>
+#define vll vector<long long>
+#define vchar vector<char>
+#define vstr vector<string>
+#define rr(x) x.begin(),x.end()
+#define nl endl
+#define str string
+#define si set<int>
+#define pb(x) push_back(x)
+#define ull unsigned long long 
+#define floop(i,a,b) for(int i=a;i<b;++i)
+#define sp " "
+#define mii map<int,int>
+#define fauto(i,x) for(auto i:x)
+
 int main(){
     int t;
     cin >> t;
