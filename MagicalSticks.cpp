4f476a3d3ce8c8f275972f0edd01c9badf6c4554
Krishna Sharma <rakesh7355249204@gmail.com>
2025-04-29 22:27:56 +0530

    MagicalSticks

---
+++ MagicalSticks.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+#include <cstring>
+#include <unordered_map>
+using namespace std;
+#define ll long long 
+#define vint vector<int>
+#define vll vector<long long>
+#define vchar vector<char>
+#define vstr vector<string>
+#define rr(x) x.begin()+1,x.end()
+#define nl endl
+#define str string
+#define si set<int>
+#define pb(x) push_back(x)
+#define ull unsigned long long 
+#define floop(i,a,b) for(int i=a;i<b;++i)
+#define sp " "
+#define mii map<int,int>
+#define fauto(i,x) for(auto i:x)
+#define vpint vector<pair<int,int> >
+#define umpint unordered_map<int,int>
+#define vvint vector<vector<int> > 
+#define maxelem(x) *max_element(x.begin(),x.end())
+#define minelem(x) *min_element(x.begin(),x.end())
+void logic(){
+    int n;
+    cin >> n;
+    if(n%2==0) cout << n/2 << nl;
+    else cout << (n+1)/2 << nl;
+}
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        logic();
+    }
+    return 0;
+}
