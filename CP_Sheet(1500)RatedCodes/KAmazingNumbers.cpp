f4387bc2362ebae61b518b10ed57385147a18a33
Krishna Sharma <rakesh7355249204@gmail.com>
2025-10-04 20:18:06 +0530

    ZeroQuantityMaximization

--- CP_Sheet(1500)RatedCodes/KAmazingNumbers.cpp
+++ CP_Sheet(1500)RatedCodes/KAmazingNumbers.cpp
@@ -11,10 +11,6 @@
 #include <algorithm>
 #include <set>
 #include <queue>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define Oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 using namespace std;
 #define ll long long 
 #define pint pair<int,int>
@@ -42,88 +38,7 @@ using namespace std;
 #define vpib vector<pair<int,bool> >
 #define maxelem(x) *max_element(x.begin(),x.end())
 #define minelem(x) *min_element(x.begin(),x.end())
-#define MOD (ll)(1e9+7)
-bool check(ll mid,vll &hg,ll x){
-    ll units=0;
-    int n=hg.size();
-    floop(i,0,n){
-        if(hg[i]<mid){
-            units += (mid-hg[i]);
-        }
-    }
-    return units<=x;
-}
-vint FindMax(vint &arr){
-    vpint tmp(arr.size());
 
-    floop(i,0,tmp.size()){
-        tmp[i].first=arr[i];
-        tmp[i].second=i;
-    }
-    sort(ds(tmp));
-    vint ans(3);
-    floop(i,0,3) ans[i]=tmp[i].second;
-    return ans;
-}
-ll calculatesum(ll start,ll end){
-    ll sum = ((start +end)*(end-start +1))/2;
-
-    return sum;
-}
-const ll mod = 1e9+7;
-bool pf(ll val,int ind,int x,int mid){
-    if(val + (1LL * (ind + 1)*(mid-1))<=x)
-        return true;
-    return false;
-}
-int binsearch(ll val,int ind,int x){
-    int low = 1,high=1e9+5;
-    int ans = 0;
-
-    while(low<=high){
-        int mid = (low + high)/2;
-        if(pf(val,ind,x,mid)){
-            ans=mid;
-            low=mid+1;
-        }else{
-            high = mid -1;
-        }
-    }
-    return ans ;
-}
-int binarysearch(vll &pmax,int n,int val){
-    int low = 0,high=n-1;
-    int ans = -1;
-
-    while(low<=high){
-        int mid = (low + high)/2;
-        if(pmax[mid]<=val){
-            ans=mid;
-            low=mid+1;
-        }else{
-            high = mid -1;
-        }
-    }
-    return ans ;
-}
-ll modl = 998244353;
-int ans=0;
-int dfs(int node,vvint &adj,str &s){
-    if(adj[node].size()==0){
-        return s[node-1]=='W'?1:-1;
-    }
-    int cnt = 0;
-    for(auto child:adj[node]){
-        cnt +=dfs(child,adj,s);
-    }
-    cnt += (s[node-1]=='W'?1:-1);
-    if(cnt==0) ans++;
-
-    return cnt;
-}
-ll calculateSumOfFirstX(int x){
-    return x*1ll*(x+1)/2;
-}
 void logic(){
     int n;
     cin >> n;
@@ -131,13 +46,13 @@ void logic(){
     fauto(i,a)
         cin >> i; 
 
-    vint pos(n + 1);
+    vvint pos(n + 1);
     floop(i,0,n){
         pos[a[i]].push_back(i);
     } 
 
     vint ans(n, -1);
-    for (int i = 1; i <= n; i++) {
+    floop(i,1,n+1){
         if (pos[i].empty())
             continue;
 
@@ -147,7 +62,7 @@ void logic(){
         }
         mx = max(mx, n - pos[i].back());
 
-            floop(j.mx-1,n){
+            floop(j,mx-1,n){
             if (ans[j] != -1)
                 break;
             ans[j] = i; 
