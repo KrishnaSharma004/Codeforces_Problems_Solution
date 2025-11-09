6bba8367e44f74f9f45a68244af5ee837ae207a2
Krishna Sharma <rakesh7355249204@gmail.com>
2025-11-09 22:18:14 +0530

    ShortSubstrings

--- ShortSubstrings.cpp
+++ ShortSubstrings.cpp
@@ -1,47 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cmath>
-#include <map>
-#include <cstring>
-#include <unordered_map>
 using namespace std;
-#define ll long long 
-#define vint vector<int>
-#define vll vector<long long>
-#define vchar vector<char>
-#define vstr vector<string>
-#define rr(x) x.begin()+1,x.end()
-#define nl endl
-#define str string
-#define si set<int>
-#define pb(x) push_back(x)
-#define ull unsigned long long 
-#define floop(i,a,b) for(int i=a;i<b;++i)
-#define sp " "
-#define mii map<int,int>
-#define fauto(i,x) for(auto i:x)
-#define vpint vector<pair<int,int> >
-#define umpint unordered_map<int,int>
+#define ll long long  
 
-void logic(){
-    str s;
-    cin>>s;
-    int n=s.size();
-    str ans="";
-    ans = ans + (char)s[0]+(char)s[1];
-    floop(i,3,n){
-        ans = ans+(char)s[i];
+void solve(){
+    string b;
+    cin >> b;
+    for(int i=0;i<b.length()-1;i+=2){
+        cout << b[i];
     }
-    cout << ans << nl;
-    return;
+    cout << b[b.length()-1] << endl;
 }
-
 int main(){
     int t;
     cin >> t;
     while(t--){
-        logic();
+        solve();
     }
     return 0;
 }
