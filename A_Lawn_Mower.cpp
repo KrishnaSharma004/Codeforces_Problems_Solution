a45560920a8f00d9a4f26772586a05004e3b3042
Krishna Sharma <rakesh7355249204@gmail.com>
2026-03-29 22:27:42 +0530

    A_Lawn_Mower

---
+++ A_Lawn_Mower.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second 
+#define pb push_back
+#define pop pop_back
+#define all(x) x.begin(), x.end()
+typedef long long ll;
+
+void solve(){
+    ll n, w;
+    cin >> n >> w;
+    ll keep = (n/w);
+    cout << n - keep << "\n";
+}
+int32_t main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
