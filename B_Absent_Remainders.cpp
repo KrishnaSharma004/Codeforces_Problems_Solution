83927ec3f10a9f5bf8d7069564570bab931784d3
Krishna Sharma <rakesh7355249204@gmail.com>
2026-05-09 17:32:20 +0530

    B_Sorted_Adjacent_Differences B_Absent_Remainders

---
+++ B_Absent_Remainders.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second 
+#define pb push_back
+#define pp pop_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int> 
+#define vll vector<ll> 
+#define si set<int> 
+#define sch set<char> 
+#define sll set<ll> 
+#define mpii map<int, int> 
+#define mpll map<ll, ll> 
+#define mpli map<ll, int> 
+#define mpil map<int, ll> 
+#define vpii vector<pair<int, int>>
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define floopinc(i,x,y) for(int i = x; i < y ; ++i)
+#define floopdec(i,x,y) for(int i = x; i >= y ; --i)
+#define fauto(i,x) for(auto &i : x)
+typedef long long ll;
+void yn(bool condition){
+    if(condition){
+        yes;
+    }else{
+        no;
+    }
+}
+void ny(bool condition) { yn(!condition);}
+
+int gcd (int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd (b, a % b);
+}
+int lcm (int a, int b) {
+    return a / gcd(a, b) * b;
+}
+void krishna(){
+    int n;
+    cin >> n;
+    vi a(n);
+    fauto(i, a) cin >> i;
+    sort(all(a));
+    floopinc(i,1,n/2+1){
+        cout << a[i] << " " << a[0] << '\n';
+    }
+}
+int32_t main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)  krishna();
+    return 0;
+}
---
+++ B_Sorted_Adjacent_Differences.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second 
+#define pb push_back
+#define pp pop_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int> 
+#define vll vector<ll> 
+#define si set<int> 
+#define sch set<char> 
+#define sll set<ll> 
+#define mpii map<int, int> 
+#define mpll map<ll, ll> 
+#define mpli map<ll, int> 
+#define mpil map<int, ll> 
+#define vpii vector<pair<int, int>>
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define floopinc(i,x,y) for(int i = x; i < y ; ++i)
+#define floopdec(i,x,y) for(int i = x; i >= y ; --i)
+#define fauto(i,x) for(auto &i : x)
+typedef long long ll;
+void yn(bool condition){
+    if(condition){
+        yes;
+    }else{
+        no;
+    }
+}
+void ny(bool condition) { yn(!condition);}
+
+int gcd (int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd (b, a % b);
+}
+int lcm (int a, int b) {
+    return a / gcd(a, b) * b;
+}
+void copyvector(vector<int> &a, vector<int> &res){
+    floopinc(i,0,a.size()){
+        res[i] = a[i];
+    }
+}
+void krishna(){
+    int n;
+    cin >> n;
+    vi a(n);
+    fauto(i,a) cin >> i;
+    sort(all(a));
+    vi res;
+    int i = n/2-1, j = n/2;
+    while(i>=0 && j < n){
+        if(n&1){
+            res.pb(a[j]);
+            res.pb(a[i]);
+            i--;j++;
+        }
+        else{
+            res.pb(a[i]);
+            res.pb(a[j]);
+            i--;j++;
+        }
+    }
+    if(j<n){
+        res.pb(a[j]);
+    }
+    fauto(i,res)cout << i << " ";
+    cout << '\n';
+}
+int32_t main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)  krishna();
+    return 0;
+}
