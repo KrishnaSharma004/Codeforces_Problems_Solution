8d5dce19d1c1458d70a007e3ad7e1115c43cf0b3
Krishna Sharma <rakesh7355249204@gmail.com>
2026-05-12 14:47:12 +0530

    A_I_Wanna_Be_The_Guy

---
+++ A_I_Wanna_Be_The_Guy.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second 
+#define pb push_back
+#define pp pop_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int> 
+#define vll vector<ll> 
+#define si set<int> 
+#define sch set<char> 
+#define sll set<ll> 
+#define mpii map<int, int> 
+#define mpll map<ll, ll> 
+#define mpli map<ll, int> 
+#define mpil map<int, ll> 
+#define vpii vector<pair<int, int>>
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define floopinc(i,x,y) for(int i = x; i < y ; ++i)
+#define floopdec(i,x,y) for(int i = x; i >= y ; --i)
+#define fauto(i,x) for(auto &i : x)
+typedef long long ll;
+void yn(bool condition){
+    if(condition){
+        yes;
+    }else{
+        no;
+    }
+}
+void ny(bool condition) { yn(!condition);}
+
+int gcd (int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd (b, a % b);
+}
+int lcm (int a, int b) {
+    return a / gcd(a, b) * b;
+}
+void copyvector(vector<int> &a, vector<int> &res){
+    floopinc(i,0,a.size()){
+        res[i] = a[i];
+    }
+}
+int32_t main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int n,p,q;
+    cin >> n;
+    cin >> p;
+    si st;
+    floopinc(i,0,p){
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+    cin >> q;
+    floopinc(i,0,q){
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+    floopinc(i,1,n+1){
+        if(st.find(i) == st.end()){
+            cout << "Oh, my keyboard!" << '\n';
+            return 0;
+        }
+    }
+    cout << "I become the guy." << '\n';
+    return 0;
+}
