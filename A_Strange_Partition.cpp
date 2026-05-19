76c8d8260c07c166eca2fc11c33dffafc0d0f68a
Krishna Sharma <rakesh7355249204@gmail.com>
2026-05-19 16:19:51 +0530

    A_Strange_Partition

---
+++ A_Strange_Partition.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second 
+#define pb push_back
+#define pp pop_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int> 
+#define vll vector<ll> 
+#define vb vector<bool> 
+#define vch vector<char>
+#define si set<int> 
+#define sch set<char> 
+#define sll set<ll> 
+#define mpii map<int, int> 
+#define mpll map<ll, ll> 
+#define mpli map<ll, int> 
+#define mpil map<int, ll> 
+#define vpii vector<pair<int, int>>
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define floopinc(i,x,y) for(int i = x; i < y ; ++i)
+#define floopdec(i,x,y) for(int i = x; i >= y ; --i)
+#define fauto(i,x) for(auto &i : x)
+typedef long long ll;
+void yn(bool condition){
+    if(condition){
+        yes;
+    }else{
+        no;
+    }
+}
+void ny(bool condition) { yn(!condition);}
+
+int gcd (int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd (b, a % b);
+}
+int lcm (int a, int b) {
+    return a / gcd(a, b) * b;
+}
+void copyvector(vector<int> &a, vector<int> &res){
+    floopinc(i,0,a.size()){
+        res[i] = a[i];
+    }
+}
+int lower_bound(vector<int> &a, int target){
+    int lo = 0, hi = a.size() - 1;
+    while(hi-lo > 0){
+        int mid = (lo+hi)/2;
+        if(a[mid] == target) return mid;
+        if(a[mid] > target) hi = mid;
+        else lo = mid + 1;
+    }
+    if(a[lo] >= target) return lo;
+    else return a.size();
+}  
+void krishna(){
+    int n, x;
+    cin >> n >> x;
+    vi a(n);
+    fauto(i,a) cin >> i;
+    ll sum = 0;
+    floopinc(i,0,n) sum+=a[i];
+    ll mx = 0;
+    floopinc(i,0,n){
+        mx += ceil(a[i]/(x*1.0));
+    }
+    cout << (ll)ceil((1.0*sum)/x) <<  " " << mx << '\n';
+}
+int32_t main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)  
+    krishna();
+    return 0;
+}
